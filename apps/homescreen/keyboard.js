05bb46ea4dfc6717b1a4f870f2d381f062870273
Timothy Guan-tin Chien <timdream@gmail.com>
2011-12-12 16:44:44 +0800

    Integrate JSZhuYing into the newly created layout

--- apps/homescreen/keyboard.js
+++ apps/homescreen/keyboard.js
@@ -84,16 +84,7 @@ const IMEManager = {
         if (evt.target.dataset.selection) {
           this.layout.select(
             evt.target.textContent,
-            evt.target.dataset.data,
-            function () {
-              that.showSelections.apply(that, arguments);
-            },
-            window.navigator.mozKeyboard.sendKey,
-            function sendString(str) {
-              for (var i = 0; i < str.length; i++) {
-                window.navigator.mozKeyboard.sendKey(str.charCodeAt(i));
-              }
-            }
+            evt.target.dataset.data
           );
           this.updateKeyboardHeight();
           return;
@@ -135,16 +126,7 @@ const IMEManager = {
           default:
             if (this.layout.type === 'ime') {
               this.layout.click(
-                keyCode,
-                function () {
-                  that.showSelections.apply(that, arguments);
-                },
-                window.navigator.mozKeyboard.sendKey,
-                function sendString(str) {
-                  for (var i = 0; i < str.length; i++) {
-                    window.navigator.mozKeyboard.sendKey(str.charCodeAt(i));
-                  }
-                }
+                keyCode
               );
               this.updateKeyboardHeight();
               break;
