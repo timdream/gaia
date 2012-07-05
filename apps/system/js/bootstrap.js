fbc0d828b4aa239dde71ff1bff89b97c636e8a86
Timothy Guan-tin Chien <timdream@gmail.com>
2012-07-05 11:11:55 +0800

    Move files

--- apps/system/js/bootstrap.js
+++ apps/system/js/bootstrap.js
@@ -4,7 +4,6 @@
 'use strict';
 
 function startup() {
-  PinLock.init();
   SoundManager.init();
   SleepMenu.init();
   SourceView.init();
--- test_apps/simlock/js/simlock.js
+++ test_apps/simlock/js/simlock.js
@@ -140,3 +140,5 @@ var PinLock = {
     }
   }
 };
+
+PinLock.init();
