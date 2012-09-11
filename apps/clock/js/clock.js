01e6413c40817995cced98ac1f2b0c8652adb85f
Timothy Guan-tin Chien <timdream@gmail.com>
2012-09-11 14:58:53 +0800

    Clock hack

--- apps/clock/js/clock.js
+++ apps/clock/js/clock.js
@@ -24,6 +24,24 @@ function choiceChanged(target) {
   view.hidden = false;
 }
 
+// XXX: Hack to trigger onring.html for test
+window.addEventListener('load', function loaded() {
+  window.removeEventListener('load', loaded);
+
+  var dayDate = document.getElementById('clock-day-date');
+  dayDate.addEventListener('click', function() {
+    alert('Hack: open up onring screen after 10 sec.');
+    setTimeout(function() {
+      AlarmManager._onFireAlarm = AlarmEditView.getDefaultAlarm();
+
+      var protocol = window.location.protocol;
+      var host = window.location.host;
+      window.open(protocol + '//' + host + '/onring.html',
+                  'ring_screen', 'attention');
+    }, 10000);
+  });
+});
+
 // Set the 'lang' and 'dir' attributes to <html> when the page is translated
 window.addEventListener('localized', function showBody() {
   document.documentElement.lang = navigator.mozL10n.language.code;
